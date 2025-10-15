7dc9f97d8c89955f9377c0a4dcb5ce0d3ca3cf1a
Abhay <abhaygupta4235@gmail.com>
2025-10-15 15:15:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// let age :number = 20;
+// if (age < 50)
+//     age += 50;
+// console.log(age);
+// let fName:string="Abhay"; 
+// let a:number=2;
+// let b:number=3;
+// let c:number=a+b;
+// console.log(c);
+//@ts-ignore
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 3));
